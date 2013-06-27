ee18776a4eb4f1276e44fda7fcb9d4d1a8c984ca
Jeff <guionj@gmail.com>
2013-06-26 23:11:12 -0400

    Two versions of map, one with addresses, the other without. Major refactor of code, removed unnecessary code and abstracted much logic to boston_map.py

--- broken_windows/urls.py
+++ broken_windows/urls.py
@@ -5,18 +5,12 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'broken_windows.views.home', name='home'),
-    # url(r'^broken_windows/', include('broken_windows.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'boston/?$', 'boston_disorder.views.crm'),
-    url(r'boston/physical/(?P<l1>[a-zA-Z0-9: _]*)/?$', 'boston_disorder.views.crm'),
-    url(r'boston/social/(?P<l1>[a-zA-Z0-9: _]*)/?$', 'boston_disorder.views.calls'),
+    url(r'boston/physical/(?P<map_type>[a-zA-Z0-9: _]*)/?$', 'boston_disorder.views.crm'),
+    url(r'boston/social/(?P<map_type>[a-zA-Z0-9: _]*)/?$', 'boston_disorder.views.calls'),
+    url(r'boston/display_map/physical/(?P<map_type>[a-zA-Z0-9: _]*)/?$', 'boston_disorder.views.limited_crm'),
+    url(r'boston/display_map/social/(?P<map_type>[a-zA-Z0-9: _]*)/?$', 'boston_disorder.views.limited_calls'),
     url(r'boston/more_info/?$', 'boston_disorder.views.more_info')
 
 )
